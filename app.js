829953f9d91b6dd8f3db2505d192efe70c7a5fd9
osamasaeed468 <osamasaeed468@github.com>
2020-12-05 16:34:10 +0500

    make it better

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ db.connect((err)=>{
     // Start up our Express Application
     // And listen for Request
     else{
-        app.listen(3000,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log('connected to database, app listening on port 3000');
         });
     }
